85be1dda985c591e9629128360100bee13b0388b
krishnavamsivendra2 <krishnavamsivendra2@gmail.com>
2025-12-25 23:57:10 +0530

    Update External Tables Creation.sql

--- gold/External Tables Creation.sql
+++ gold/External Tables Creation.sql
@@ -1,13 +1,18 @@
+CREATE MASTER KEY ENCRYPTION BY PASSWORD ='<password>' 
+
+CREATE DATABASE SCOPED CREDENTIAL <credential_name>
+WITH IDENTITY = 'identity_name'
+
 CREATE EXTERNAL DATA SOURCE silver_source
 WITH(
-    LOCATION = 'https://krishnavamsi.dfs.core.windows.net/silver',
-    CREDENTIAL = cred_vamsi_adventure_works
+    LOCATION = 'https://<storage_account_name>.dfs.core.windows.net/<silver_container_name>',
+    CREDENTIAL = <credential_name>
 )
 
 CREATE EXTERNAL DATA SOURCE gold_dest
 WITH(
-    LOCATION = 'https://krishnavamsi.dfs.core.windows.net/silver',
-    CREDENTIAL = cred_vamsi_adventure_works
+    LOCATION = 'https://<storage_account_name>.dfs.core.windows.net/<gold_container_name>',
+    CREDENTIAL = <credential_name>
 )
 
 CREATE EXTERNAL FILE FORMAT gold_format
@@ -19,9 +24,73 @@ WITH(
 
 CREATE EXTERNAL TABLE gold.extsales
 WITH(
-    LOCATION = 'extsales',
+    LOCATION = 'extsales/',
     DATA_SOURCE = gold_dest,
     FILE_FORMAT = gold_format
 )
 AS
 SELECT * from gold.sales;
+
+CREATE EXTERNAL TABLE gold.extcustomers
+WITH(
+    LOCATION = 'extcustomers/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.customers;
+
+CREATE EXTERNAL TABLE gold.extproduct_categories
+WITH(
+    LOCATION = 'extproduct_categories/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.product_categories;
+
+CREATE EXTERNAL TABLE gold.extproduct_subCategories
+WITH(
+    LOCATION = 'extproduct_subCategories/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.product_subCategories;
+
+CREATE EXTERNAL TABLE gold.extproducts
+WITH(
+    LOCATION = 'extproducts/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.products;
+
+CREATE EXTERNAL TABLE gold.extreturns
+WITH(
+    LOCATION = 'extreturns/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.returns;
+
+CREATE EXTERNAL TABLE gold.extsales
+WITH(
+    LOCATION = 'extsales/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.sales;
+
+CREATE EXTERNAL TABLE gold.extterritories
+WITH(
+    LOCATION = 'extterritories/',
+    DATA_SOURCE = gold_dest,
+    FILE_FORMAT = gold_format
+)
+AS
+SELECT * from gold.territories;
+
